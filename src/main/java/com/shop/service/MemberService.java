fa659104afe2b8c6a0ccad3c9a4c1ab2d84ad652
SangHun <shzero211@naver.com>
2022-02-11 16:19:28 +0900

    [ADD]로그인 구현

--- src/main/java/com/shop/service/MemberService.java
+++ src/main/java/com/shop/service/MemberService.java
@@ -1,5 +1,9 @@
 package com.shop.service;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,7 +15,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 private final MemberRepository memberRepository;
 
 public Member saveMember(Member member) {
@@ -24,4 +28,17 @@ public void duplicate(Member member) {
 		throw new IllegalStateException("이미 가입된 회원 입니다.");
 	}
 }
+@Override
+public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+	Member member =memberRepository.findByEmail(email);
+	if(member==null) {
+		throw new UsernameNotFoundException(email);
+	}
+	return User.builder()
+						.username(member.getEmail())
+						.password(member.getPassword())
+						.roles(member.getRole().toString())
+						.build();
+}
+
 }
