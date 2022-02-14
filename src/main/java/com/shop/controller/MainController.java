df9712d6b47e4fb9ea280f1c5424aa972c900fc9
SangHun <shzero211@naver.com>
2022-02-14 16:43:38 +0900

    [ADD] memberLogin[ADD]memberForm

--- src/main/java/com/shop/controller/MainController.java
+++ src/main/java/com/shop/controller/MainController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class thymeleaf {
-@GetMapping(value = "/members/login")
-public String thymeleaf1() {
-	return "/layouts/layout1";
+public class MainController {
+@GetMapping(value = "/")
+public String main() {
+	return "main";
 }
 }
