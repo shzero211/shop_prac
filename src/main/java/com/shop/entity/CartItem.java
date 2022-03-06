a995b675c62b36bbff1542150126d4d9929a592d
SangHun <shzero211@naver.com>
2022-03-06 21:09:23 +0900

    [ADD]Cart

--- src/main/java/com/shop/entity/CartItem.java
+++ src/main/java/com/shop/entity/CartItem.java
@@ -31,4 +31,20 @@ private Cart cart;
 private Item item;
 
 private int count;
+
+public static CartItem createCartItem(Cart cart,Item item,int count) {
+	CartItem cartItem=new CartItem();
+	cartItem.setCart(cart);
+	cartItem.setItem(item);
+	cartItem.setCount(count);
+	return cartItem;
+}
+
+public void addCount(int count) {
+	this.count+=count;
+}
+
+public void updateCount(int count) {
+	this.count=count;
+}
 }
